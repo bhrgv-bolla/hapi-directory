fdf57e108efbfb34bea73f2cf04bafb2d0260a0d
Bhargav Bolla <hire.bhargav@gmail.com>
2018-03-26 17:26:32 -0700

    added code to zip directory.

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+    Decorate the hapi server with defining methods on the handler, toolkit??
+
+https://hapijs.com/api/16.6.3#serverdecoratetype-property-method-options
+    Call the this.file(path, {confine: false}) to invoke the inert functionality.
+
+    this => reply.
+ */
+
+const {zipDirectory} = require("./lib/archive-helper");
+const handler = (directoryPath) => {
+
+}
